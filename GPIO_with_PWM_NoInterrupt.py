f1dad524dab345157198b345ae4f60b287e19d15
Seo Hwi <chromatic_365@naver.com>
2022-07-03 22:28:19 +0900

    220703 라즈베리파이 코드 추가
      PWM 실행 중 각 스위치별 동작이 실행되도록 경우를 분할했음.
    
      유닛의 발광 시간을 결정하는 변수는 SEC로 우선은 2진 조합이 아닌 5초로 고정해뒀음.
    
      만약 스위치를 2개 이상 누르거나 에러의 경우 else의 경우로 그냥 pass하도록 설정했음.

--- GPIO_with_PWM_NoInterrupt.py
+++ GPIO_with_PWM_NoInterrupt.py
@@ -4,6 +4,7 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
+#==================================================================
 GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #switch1
 GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #switch2
 GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #switch3
@@ -23,18 +24,29 @@ GPIO.setup(20, GPIO.OUT) #Unit6
 GPIO.setup(21, GPIO.OUT) #Unit7
 
 GPIO.setup(14, GPIO.OUT) #Tower
+#===================================================================
+
+GPIO.output(14, True) #Tower는 항상 On 상태
 
 WAVE=GPIO.PWM(13, 6000)
 WAVE.start(0)
 
-SELECT="wave"
+SEC=5
+
+SELECT="wave" #초기 상태 선언
 
 try:
     while(True):
         if SELECT=="wave":
             for DUTY in range(1, 101, 10):
                 SW1=GPIO.input(17)
-                if SW1==1:
+                SW2=GPIO.input(27)
+                SW3=GPIO.input(22)
+                SW4=GPIO.input(5)
+                SW5=GPIO.input(6)
+                SW6=GPIO.input(19)
+                SW7=GPIO.input(26)
+                if SW1==1 or SW2==1 or SW3==1 or SW4==1 or SW5==1 or SW6==1 or SW7==1:
                     SELECT="unit"
                     break
                 WAVE.ChangeDutyCycle(DUTY)
@@ -42,17 +54,52 @@ try:
                 time.sleep(0.1)
                 
             for DUTY in range(99, 1, -10):
-                if SW1==1:
+                SW1=GPIO.input(17)
+                SW2=GPIO.input(27)
+                SW3=GPIO.input(22)
+                SW4=GPIO.input(5)
+                SW5=GPIO.input(6)
+                SW6=GPIO.input(19)
+                SW7=GPIO.input(26)
+                if SW1==1 or SW2==1 or SW3==1 or SW4==1 or SW5==1 or SW6==1 or SW7==1:
                     SELECT="unit"
                     break
                 WAVE.ChangeDutyCycle(DUTY)
                 GPIO.output(13, DUTY)
                 time.sleep(0.1)
+                
         if SELECT=="unit": 
             WAVE.ChangeDutyCycle(0)
-            GPIO.output(23, True)
-            time.sleep(5)
-            GPIO.output(23, False)
+            if SW1==1:
+                GPIO.output(23, True)
+                time.sleep(SEC)
+                GPIO.output(23, False)
+            if SW2==1:
+                GPIO.output(24, True)
+                time.sleep(SEC)
+                GPIO.output(24, False)
+            if SW3==1:
+                GPIO.output(25, True)
+                time.sleep(SEC)
+                GPIO.output(25, False)
+            if SW4==1:
+                GPIO.output(12, True)
+                time.sleep(SEC)
+                GPIO.output(12, False)
+            if SW5==1:
+                GPIO.output(16, True)
+                time.sleep(SEC)
+                GPIO.output(16, False)
+            if SW6==1:
+                GPIO.output(20, True)
+                time.sleep(SEC)
+                GPIO.output(20, False)
+            if SW7==1:
+                GPIO.output(21, True)
+                time.sleep(SEC)
+                GPIO.output(21, False)
+            else:
+                pass
             SELECT="wave"
 
 except KeyboardInterrupt:
